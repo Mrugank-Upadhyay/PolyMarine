79f871e89f08a4491f637a0fecea1e1097ccc511
Mihran Mashhud <mihranmashhud@gmail.com>
2021-10-03 13:42:41 -0400

    add loop for time

--- scripts/particle_sym.py
+++ scripts/particle_sym.py
@@ -1,6 +1,5 @@
+import datetime
 from osgeo import ogr, osr
-
-
 
 def getCoordConverter(src='', targ=''):
     srcproj = osr.SpatialReference()
@@ -23,3 +22,13 @@ def getCoordConverter(src='', targ=''):
 latlongToAlbers = getCoordConverter(4326,5070)
 albersToLatlong = getCoordConverter(5070,4326)
 
+start_date = datetime.datetime(1992,1,1)
+end_date = datetime.datetime(1992,2,1)
+
+current_date = start_date
+increment = datetime.timedelta(hours=1)
+
+while current_date < end_date:
+
+    current_date += increment
+    pass
