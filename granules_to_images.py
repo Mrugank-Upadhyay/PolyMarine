3f1c9034997f51339a45ea70efbf6ccf3c99aa46
Mihran Mashhud <mihranmashhud@gmail.com>
2021-10-02 23:07:49 -0400

    loop over all data.

--- granules_to_images.py
+++ granules_to_images.py
@@ -9,48 +9,39 @@ def map_range(input_start,input_end,output_start,output_end,val):
     slope = (output_end - output_start) / (input_end - input_start)
     return output_start + slope * (val - input_start)
 
-year_month = "1992-01"
+for year in range(1992,2018):
+    for month in range(1,13):
+        ecco_nc_file = f"OCEAN_VELOCITY_mon_mean_{str(year).zfill(4)}-{str(month).zfill(2)}_ECCO_V4r4_latlon_0p50deg.nc"
+        nc = netCDF4.Dataset(ecco_nc_file, mode="r")
+        nc.variables.keys()
+        lat_bnds = nc.variables['latitude_bnds'][:]
+        long_bnds = nc.variables['longitude_bnds'][:]
+        time_bnds_var = nc.variables['time_bnds']
+        time_var = nc.variables['time']
+        dtime = netCDF4.num2date(time_var[:],time_var.units)
+        evel = nc.variables['EVEL']
+        nvel = nc.variables['NVEL']
 
-ecco_nc_file = f"OCEAN_VELOCITY_mon_mean_2017-12_ECCO_V4r4_latlon_0p50deg.nc"
-nc = netCDF4.Dataset(ecco_nc_file, mode="r")
+        min_vel = -2
+        max_vel = 2
 
-nc.variables.keys()
+        w = 720
+        h = 360
+        pic_size = (h,w,3)
 
-lat_bnds = nc.variables['latitude_bnds'][:]
-long_bnds = nc.variables['longitude_bnds'][:]
-time_bnds_var = nc.variables['time_bnds']
-time_var = nc.variables['time']
-dtime = netCDF4.num2date(time_var[:],time_var.units)
-evel = nc.variables['EVEL']
-nvel = nc.variables['NVEL']
-# evel is 360x50
+        for i, (e_arr,n_arr) in enumerate(zip(evel[:][0],nvel[:][0])):
+            filename = f"test{i}.png"
+            out_arr = np.zeros(pic_size,dtype=np.int8)
+            for y, (e_row,n_row) in enumerate(zip(reversed(e_arr), reversed(n_arr))):
+                for x, (e_val,n_val) in enumerate(zip(e_row,n_row)):
+                    if e_val.dtype == 'float64':
+                        out_arr[y,x] = [0,100,0]
+                        continue
+                    mapped_e_val = math.floor(map_range(min_vel, max_vel,0,255,e_val))
+                    mapped_n_val = math.floor(map_range(min_vel, max_vel,0,255,n_val))
+                    out_arr[y,x] = [mapped_e_val,0,mapped_n_val]
 
-print(evel)
-print(nvel)
-
-min_vel = -2
-max_vel = 2
-
-w = 720
-h = 360
-pic_size = (h,w,3)
-
-# out = cv2.VideoWriter('ecco_2017_12.avi',cv2.VideoWriter_fourcc(*'DIVX'), 10, (w,h))
-
-for i, (e_arr,n_arr) in enumerate(zip(evel[:][0],nvel[:][0])):
-    filename = f"test{i}.png"
-    out_arr = np.zeros(pic_size,dtype=np.int8)
-    for y, (e_row,n_row) in enumerate(zip(reversed(e_arr), reversed(n_arr))):
-        for x, (e_val,n_val) in enumerate(zip(e_row,n_row)):
-            if e_val.dtype == 'float64':
-                out_arr[y,x] = [0,100,0]
-                continue
-            mapped_e_val = math.floor(map_range(min_vel, max_vel,0,255,e_val))
-            mapped_n_val = math.floor(map_range(min_vel, max_vel,0,255,n_val))
-            out_arr[y,x] = [mapped_e_val,0,mapped_n_val]
-
-    img = Image.fromarray(out_arr,"RGB")
-    filename = f"ecco_2017_12_{str(i).zfill(3)}.png"
-    img.save(filename)
-    break
-
+            img = Image.fromarray(out_arr,"RGB")
+            filename = f"images/ecco_2017_12_{str(i).zfill(3)}.png"
+            img.save(filename)
+            break
